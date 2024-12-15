22a498d468a55ed88e84623dac9ec6ae6180a7ae
Amange1di <amangeldi2702006@gmail.com>
2024-12-15 10:37:33 +0600

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Name from '../../components/name/Name'
+import "./Home.css"
+import AddNameForm from '../../components/name/AddName'
+const Home = () => {
+  return (
+    <div>
+      <h1>.</h1>
+      <AddNameForm />
+      <Name />
+    </div>
+  )
+}
+
+export default Home
